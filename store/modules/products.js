3e5bce5782653361199bac0386aa005b2fac860d
DESKTOP-7ORT8AH\jaker <jawad@vonome.com>
2021-08-22 11:29:56 +0600

    Products state uncommented

--- store/modules/products.js
+++ store/modules/products.js
@@ -1,30 +1,30 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// export const state ={
-//   products:[]
-// };
+export const state ={
+  products:[]
+};
 
-// export const getters ={
-  //allProducts:state=>state.products
-//};
+export const getters ={
+  allProducts:state=>state.products
+};
 
-// export const actions ={
-//   async getProducts({commit}){
-//     const response = await axios.get('http://localhost:4000/products');
-//     console.log("res> "+response.data);
-//     commit("addProducts", products);
-//     return products;
-//   }
-// };
-// const mutations ={
-//   addProducts(state,products){
-//     state.products.push({...products})
-//   }
-// };
+export const actions ={
+  async getProducts({commit}){
+    const response = await axios.get('http://localhost:4000/products');
+    console.log("res> "+response.data);
+    commit("addProducts", products);
+    return products;
+  }
+};
+const mutations ={
+  addProducts(state,products){
+    state.products.push({...products})
+  }
+};
 
-// export default{
-//   state,
-//   getters,
-//   actions,
-//   mutations
-// }
+export default{
+  state,
+  getters,
+  actions,
+  mutations
+}
